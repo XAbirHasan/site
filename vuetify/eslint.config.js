74100a9cc09a47613823e887c44f2263a963dbe3
XAbirHasan <abirhassanalrabbi4005@gmail.com>
2025-07-15 06:16:22 +0600

    Update gitignore and config files

--- vuetify/eslint.config.js
+++ vuetify/eslint.config.js
@@ -1,3 +1,20 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from 'eslint-config-vuetify';
 
-export default vuetify()
+export default vuetify([
+  {
+    rules: {
+      'semi': ['warn', 'always'],
+      '@stylistic/semi': 'off',
+      'perfectionist/sort-imports': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/script-indent': ['error', 2, {
+        baseIndent: 0,
+        switchCase: 0,
+      }],
+    },
+  },
+]);
